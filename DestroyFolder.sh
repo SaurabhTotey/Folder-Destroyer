62a34c5ca13de3fc81649a44706a1b37b071a6fd
Saurabh Totey <SaurabhTotey@gmail.com>
2019-05-20 17:23:46 -0600

    Started making the folder creation code

--- DestroyFolder.sh
+++ DestroyFolder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# -lt 4 ]; then
+if [ $# -lt 3 ]; then
 	echo "Program is called as such: '$0 [Path to Folder to Destroy] [Number of Options Per Layer] [Number of Layers of Folders]'."
 fi
 
@@ -9,12 +9,26 @@ if [ "$1" == "" ]; then
 	exit 1;
 fi
 
-if [ "$2" == "" ]; then
+if [ "$2" == "" ]; then #TODO: check that $2 is an int
 	echo "Must pass in how many folders the seeker must look through per layer.";
 	exit 1;
 fi
 
-if [ "$3" == "" ]; then
+if [ "$3" == "" ]; then #TODO: check that $3 is an int
 	echo "Must pass in a depth for how many folders or layers deep the destroyed folder will be.";
 	exit 1;
 fi
+
+#TODO: below only generates folders for a single path
+eval "mkdir DestroyedFolder"
+eval "cd DestroyedFolder"
+for i in {1 .. $(($3))}
+do
+	while [ $(ls -1 | wc -l) -lt $(($2)) ]
+	do
+		eval "mkdir $(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32)"
+	done
+	eval "shopt -s nullglob"
+	dirs=(*/)
+	[[ $dirs ]] && cd -- "${dirs[RANDOM%${#dirs[@]}]}"
+done
