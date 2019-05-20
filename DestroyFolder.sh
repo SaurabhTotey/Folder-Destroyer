be7f8a1dfefecf2783d33b956fb85776b646ecd1
Saurabh Totey <SaurabhTotey@gmail.com>
2019-05-20 16:47:36 -0600

    Updated help text

--- DestroyFolder.sh
+++ DestroyFolder.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+if [ $# -lt 4 ]; then
+	echo "Program is called as such: '$0 [Path to Folder to Destroy] [Number of Options Per Layer] [Number of Layers of Folders]'."
+fi
+
 if [ "$1" == "" ]; then
-	echo "Must pass in a path for a folder to destroy";
+	echo "Must pass in a path for a folder to destroy.";
 	exit 1;
 fi
 
 if [ "$2" == "" ]; then
-	echo "Must pass in how many options the seeker must look through";
+	echo "Must pass in how many folders the seeker must look through per layer.";
 	exit 1;
 fi
 
 if [ "$3" == "" ]; then
-	echo "Must pass in a depth for how many folders deep the destroyed folder will be";
+	echo "Must pass in a depth for how many folders or layers deep the destroyed folder will be.";
 	exit 1;
 fi
